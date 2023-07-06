5f4a5ad250ad0241c972a1edb71f6eac2ede72d9
javaTpoint <javatpoint@xyz>
2023-07-06 13:55:50 +0300

    first commit

---
+++ app/src/main/java/net/simplifiedcoding/navigation/AppNavHost.kt
@@ -0,0 +1,36 @@
+package net.simplifiedcoding.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import net.simplifiedcoding.ui.auth.AuthViewModel
+import net.simplifiedcoding.ui.auth.LoginScreen
+import net.simplifiedcoding.ui.auth.SignupScreen
+import net.simplifiedcoding.ui.home.HomeScreen
+
+@Composable
+fun AppNavHost(
+    viewModel: AuthViewModel,
+    modifier: Modifier = Modifier,
+    navController: NavHostController = rememberNavController(),
+    startDestination: String = ROUTE_LOGIN
+) {
+    NavHost(
+        modifier = modifier,
+        navController = navController,
+        startDestination = startDestination
+    ) {
+        composable(ROUTE_LOGIN) {
+            LoginScreen(viewModel, navController)
+        }
+        composable(ROUTE_SIGNUP) {
+            SignupScreen(viewModel, navController)
+        }
+        composable(ROUTE_HOME) {
+            HomeScreen(viewModel, navController)
+        }
+    }
+}
